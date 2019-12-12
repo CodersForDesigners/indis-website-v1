a92da5f7ec77e4a410647ff896af58d3c2f82ed6
adi <adityabhat@lazaro.in>
2019-12-12 12:35:46 +0530

    BFS : Routing : Use PHP for routing, stop using Apache's rewrite engine

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php
-	
-	// brownies are delicious
-	echo ".htaccess File does not exist or is not configured properly";
+
+
+
+
+/*
+ * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+ *  Set some useful variables
+ * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+ */
+$documentRoot = $_SERVER[ 'DOCUMENT_ROOT' ];
+$homePage = $documentRoot . '/pages/home.php';
+$_requestPathParts = explode( '?', $_SERVER[ 'REQUEST_URI' ], -1 );
+$requestPath = trim( $_requestPathParts[ 0 ] ?? $_SERVER[ 'REQUEST_URI' ], '/' );
+
+
+
+/*
+ * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+ *  Route the request to the correct file
+ * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+ */
+// Home page
+if ( $requestPath === '' )
+	return require_once $homePage;
+
+// Every other page
+$filename = $documentRoot . '/pages/' . $requestPath . '.php';
+if ( file_exists( $filename ) ) {
+	// Set a query param
+	$_GET[ '_slug' ] = $requestPath;
+	return require_once $filename;
+}
+else
+	return header( 'Location: /', true, 302 );
