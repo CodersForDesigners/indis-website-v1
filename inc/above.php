6b41e1dcbd1275110c253b5cb8ace24c8fa46071
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-01-08 13:24:56 +0530

    BUG : Some PHP warnings and notices are showing from accessing the page image

--- inc/above.php
+++ inc/above.php
@@ -18,7 +18,10 @@ $urlSlug = $_GET[ '_slug' ] ?? null;
 $postType = $_GET[ '_post_type' ] ?? null;
 
 $pageImage = getContent( '', 'page_image', $urlSlug ) ?: getContent( '', 'page_image' );
-$pageImage = $pageImage[ 'sizes' ][ 'medium' ] ?: $pageImage[ 'sizes' ][ 'thumbnail' ] ?: $pageImage[ 'url' ];
+if ( ! empty( $pageImage[ 'sizes' ] ) )
+	$pageImage = $pageImage[ 'sizes' ][ 'medium' ] ?: $pageImage[ 'sizes' ][ 'thumbnail' ] ?: $pageImage[ 'url' ];
+else
+	$pageImage = $pageImage[ 'url' ] ?? null;
 
 // #fornow
 // Just so that when some social media service (WhatsApp) try to ping URL,
