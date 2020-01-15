34634f4ca55dcae16f289957881a9f4001b55be2
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-01-15 15:30:32 +0530

    Project Page : CMS-ify this page

--- pages/projects.php
+++ pages/projects.php
@@ -1,12 +1,13 @@
 <?php
 /*
  *
- * This is a sample page you can copy and use as boilerplate for any new page.
+ * The is the Project template page
  *
  */
-require_once __DIR__ . '/../inc/above.php';
+if ( empty( $urlSlug ) )
+	return header( 'Location: /', true, 302 );
 
-// Page-specific preparatory code goes here.
+require_once __DIR__ . '/../inc/above.php';
 
 ?>
 
@@ -14,38 +15,28 @@ require_once __DIR__ . '/../inc/above.php';
 
 
 
-<!-- Sample Section -->
-<section class="sample-section">
-	<div class="container">
-		<div class="row">
-			<div class="columns small-12">
-			</div>
-		</div>
-	</div>
-</section>
-<!-- END: Sample Section -->
-
 <!-- Cover Section -->
 <section class="cover-section space-25-top space-50-bottom">
+	<?php $coverImages = getContent( [ ], 'cover_images' ); ?>
 	<div class="container">
 		<div class="row">
-			<div class="cover-image-container image-1 columns small-12 large-12"><div class="cover-image fill-neutral-2" style="background-image: url('../media/project-cover/1.jpg<?php echo $ver ?>');"></div></div>
+			<div class="cover-image-container image-1 columns small-12 large-12"><div class="cover-image fill-neutral-2": url( '<?= $coverImages[ 0 ][ 'sizes' ][ 'large' ] ?>' );"></div></div>
 			<div class="project-card columns small-8 medium-4 fill-dark space-25">
 				<div class="logo space-min-bottom"><img class="block" src="../media/indis-logo-light.svg<?php echo $ver ?>"></div>
-				<div class="title h4 strong">PBEL City, HYD</div>
-				<div class="location label strong text-uppercase text-neutral-4">Appa Junction</div>
+				<div class="title h4 strong"><?= $thePost->post_title ?></div>
+				<div class="location label strong text-uppercase text-neutral-4"><?= getContent( '', 'location' ) ?></div>
 				<hr class="dash">
-				<div class="type h6 strong space-min-top">2 & 3 BHK Apartments</div>
-				<div class="price h5 condensed">80Lakhs to 1.5Cr</div>
+				<div class="type h6 strong space-min-top"><?= getContent( '', 'type' ) ?></div>
+				<div class="price h5 condensed"><?= getContent( '', 'price' ) ?></div>
 			</div>
 			<div class="cover-image-strip columns small-4 medium-2 large-6">
 				<div class="row">
-					<div class="cover-image-container image-2 columns small-12 large-4"><div class="cover-image fill-neutral-2 js_modal_trigger cursor-pointer" tabindex="-1" data-mod-id="image-gallery" style="background-image: url('../media/project-cover/2.jpg<?php echo $ver ?>');"></div></div>
-					<div class="cover-image-container image-3 columns small-12 large-4"><div class="cover-image fill-neutral-2 js_modal_trigger cursor-pointer" tabindex="-1" data-mod-id="image-gallery" style="background-image: url('../media/project-cover/3.jpg<?php echo $ver ?>');"></div></div>
-					<div class="cover-image-container image-4 columns small-12 large-4"><div class="cover-image fill-neutral-2 js_modal_trigger cursor-pointer" tabindex="-1" data-mod-id="image-gallery" style="background-image: url('../media/project-cover/4.jpg<?php echo $ver ?>');"></div></div>
+					<div class="cover-image-container image-2 columns small-12 large-4"><div class="cover-image fill-neutral-2 js_modal_trigger x="-1" data-mod-id="image-gallery" style="background-image: url( '<?= $coverImages[ 1 ][ 'sizes' ][ 'small' ] ?>' );"></div></div>
+					<div class="cover-image-container image-3 columns small-12 large-4"><div class="cover-image fill-neutral-2 js_modal_trigger x="-1" data-mod-id="image-gallery" style="background-image: url( <?= $coverImages[ 2 ][ 'sizes' ][ 'small' ] ?> );"></div></div>
+					<div class="cover-image-container image-4 columns small-12 large-4"><div class="cover-image fill-neutral-2 js_modal_trigger x="-1" data-mod-id="image-gallery" style="background-image: url( '<?= $coverImages[ 3 ][ 'sizes' ][ 'small' ] ?>' );"></div></div>
 				</div>
 			</div>
-			<div class="cover-image-container image-5 columns small-12 medium-6 large-4"><div class="cover-image portrait fill-neutral-2 js_modal_trigger cursor-pointer" tabindex="-1" data-mod-id="image-gallery" style="background-image: url('../media/project-cover/5.jpg<?php echo $ver ?>');"><div class="icon-button zoom" style="background-image: url('../media/icon/icon-zoom.svg<?php echo $ver ?>');"></div></div></div>
+			<div class="cover-image-container image-5 columns small-12 medium-6 large-4"><div class="cover-image portrait fill-neutral-2 js_modal_trigger x="-1" data-mod-id="image-gallery" style="background-image: url( '<?= $coverImages[ 4 ][ 'sizes' ][ 'medium' ] ?>' );"><div class="icon-button zoom" style="background-image: url('../media/icon/icon-zoom.svg<?php echo $ver ?>');"></div></div></div>
 		</div>
 	</div>
 </section>
@@ -57,16 +48,13 @@ require_once __DIR__ . '/../inc/above.php';
 	<div class="container">
 		<div class="row">
 			<div class="columns small-12 medium-10 large-6 large-offset-1">
-				<div class="project-logo space-25-bottom"><img class="block" src="../media/project-logo/pbel-city.png<?php echo $ver ?>"></div>
-				<div class="title h2 strong text-lowercase space-25-bottom">Your commute to Hitec City or Financial district is <span class="text-red-2">just a 15 to 17 minute drive</span> on the Nehru Outer Ring Road.</div>
-				<div class="description text-neutral-2 space-25-bottom">Body Copy if required. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</div>
+				<div class="project-logo space-25-bottom"><img class="block" src="<?= getContent( '', 'project_logo -> sizes -> thumbnail' ) ?>"></div>
+				<div class="title h2 strong text-lowercase space-25-bottom"><?= getContent( '', 'intro_title' ) ?></div>
+				<div class="description text-neutral-2 space-25-bottom"><?= getContent( '', 'intro_description' ) ?></div>
 				<div class="points row space-50-bottom">
-					<div class="point h5 condensed columns small-12 medium-6 space-min-bottom"><span class="inline-middle space-min-right"><img src="https://via.placeholder.com/32"></span>1208sft to 1979sft</div>
-					<div class="point h5 condensed columns small-12 medium-6 space-min-bottom"><span class="inline-middle space-min-right"><img src="https://via.placeholder.com/32"></span>Ready to move-in</div>
-					<div class="point h5 condensed columns small-12 medium-6 space-min-bottom"><span class="inline-middle space-min-right"><img src="https://via.placeholder.com/32"></span>1800+ Families Live Here</div>
-					<div class="point h5 condensed columns small-12 medium-6 space-min-bottom"><span class="inline-middle space-min-right"><img src="https://via.placeholder.com/32"></span>Near the Airport</div>
-					<div class="point h5 condensed columns small-12 medium-6 space-min-bottom"><span class="inline-middle space-min-right"><img src="https://via.placeholder.com/32"></span>Swimming Pool</div>
-					<div class="point h5 condensed columns small-12 medium-6 space-min-bottom"><span class="inline-middle space-min-right"><img src="https://via.placeholder.com/32"></span>Fully Equiped Gymnasium</div>
+					<?php foreach ( getContent( [ ], 'intro_points' ) as $point ) : ?>
+						<div class="point h5 condensed columns small-12 medium-6 space-min-bottom"><span class="inline-middle space-min-right"><img src="<?= $point[ 'intro_point_icon' ] ?: 'https://via.placeholder.com/32' ?>"></span><?= $point[ 'intro_point_text' ] ?></div>
+					<?php endforeach; ?>
 				</div>
 			</div>
 		</div>
@@ -495,13 +483,13 @@ require_once __DIR__ . '/../inc/above.php';
 <section id="location" class="location-section space-50-top-bottom">
 	<div class="container">
 		<div class="row">
-			<div class="location-image-container columns small-12 large-12"><div class="location-image fill-neutral-2" style="background-image: url('../media/location-image/1.png<?php echo $ver ?>');"></div></div>
+			<div class="location-image-container columns small-12 large-12"><div class="location-image fill-neutral-2" style="background-image: url( '<?= getContent( '', 'location_panorama_static_image -> sizes -> large' ) ?>' );"></div></div>
 			<div class="location-card columns small-8 medium-4 fill-light space-25">
 				<div class="title h3 strong text-lowercase">Location</div>
-				<div class="location label strong text-uppercase text-neutral-2">Appa Junction</div>
+				<div class="location label strong text-uppercase text-neutral-2"><?= getContent( '', 'location' ) ?></div>
 				<hr class="dash">
-				<div class="address h6 text-neutral-2 space-min-top">Nehru Outer Ring Rd, Power Welfare Society, Kokapet, HYD, Telangana — 500075</div>
-				<a href="" class="label strong text-red-2 text-uppercase space-min-top-bottom inline-middle">Open in Google Maps <img class="link-icon inline-middle" src="../media/icon/icon-location-color.svg<?php echo $ver ?>"></a>
+				<div class="address h6 text-neutral-2 space-min-top">><?= getContent( '', 'location_address' ) ?></div>
+				<a href="<?= getContent( '', 'location_google_maps_url' ) ?>" class="label strong text-red-2 text-uppercase space-min-top-bottom inline-middle">Open in Google Maps <img class="link-icon inline-middle" src="../media/icon/icon-location-color.svg<?php echo $ver ?>"></a>
 			</div>
 			<div class="location-title h2 strong text-red-1 text-lowercase columns small-9 medium-5 xlarge-4"><span class="text-light">The best views</span> for 2 to 3kms in all directions</div>
 		</div>
@@ -515,7 +503,7 @@ require_once __DIR__ . '/../inc/above.php';
 	<div class="container">
 		<div class="row">
 			<div class="brochure-mockup columns small-12 medium-5 large-6 inline-middle">
-				<img class="block" src="../media/mockup-download-brochure.png<?php echo $ver ?>">
+				<img class="block" src="<?= getContent( '', 'brochure_thumbnail -> sizes -> medium' ) ?>">
 			</div>
 			<div class="brochure-action fill-light columns small-10 small-offset-1 medium-7 medium-offset-0 large-6 inline-middle">
 				<div class="download-title h3 text-lowercase strong space-25-bottom">If you're in a hurry, <br>just <span class="text-red-2">download the <span class="text-uppercase">PDF</span> brochure</span></div>
@@ -541,19 +529,19 @@ require_once __DIR__ . '/../inc/above.php';
 			<div class="points columns small-12 medium-5 large-3 large-offset-1">
 				<div class="title h4 strong space-25-bottom"><span class="text-red-2">High-Rise Build</span> <br>Engineering</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
-					The monolithic structure is stronger, has a longer life and is more earthquake resistant. 
+					The monolithic structure is stronger, has a longer life and is more earthquake resistant.
 				</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
 					Unified build process. Structure, walls, electrical and plumbing are installed and built simultaneously.
 				</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
-					No need for plastering because the wall surface is smooth and precise. 
+					No need for plastering because the wall surface is smooth and precise.
 				</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
-					Pleasing and functional interior walls that don’t have visible beams and columns. 
+					Pleasing and functional interior walls that don’t have visible beams and columns.
 				</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
-					The factory-like repetitive ‘build and check’ process ensures predictable quality. 
+					The factory-like repetitive ‘build and check’ process ensures predictable quality.
 				</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
 					Concrete is an excellent insulator. Our buildings are cooler and have better sound insulation.
@@ -662,8 +650,8 @@ require_once __DIR__ . '/../inc/above.php';
 					<div tabindex="-1" class="h5 tab-button tab-button-large">Masterplan</div>
 					<div tabindex="-1" class="h5 tab-button tab-button-large active">Block Floorplans</div>
 					<div tabindex="-1" class="h5 tab-button tab-button-large">Aerial View</div>
-				</div> 
-					
+				</div>
+
 				<select class="select-menu button strong fill-red-2 show-for-medium ">
 					<option>Masterplan</option>
 					<option selected="selected">Block Floorplans</option>
@@ -685,7 +673,7 @@ require_once __DIR__ . '/../inc/above.php';
 					</select>
 				</div>
 				<div class="plan-viewer">
-					<iframe class="plan-embed js_plan_embed" src="http://139.59.39.166/media/lazaro-office-3d-tour/index.htm" frameborder="0"></iframe>
+					<iframe class="plan-embed js_plan_embed" src="http://139.59.39.166/media/masterplans/test/Masterplan.html" frameborder="0"></iframe>
 				</div>
 			</div>
 		</div>
@@ -749,8 +737,8 @@ require_once __DIR__ . '/../inc/above.php';
 					<div tabindex="-1" class="h5 tab-button tab-button-large">Indoor</div>
 					<div tabindex="-1" class="h5 tab-button tab-button-large">Outdoor</div>
 					<div tabindex="-1" class="h5 tab-button tab-button-large active">Clubhouse</div>
-				</div> 
-					
+				</div>
+
 				<select class="select-menu button strong fill-red-2 show-for-medium ">
 					<option>Indoor</option>
 					<option>Outdoor</option>
@@ -849,7 +837,7 @@ require_once __DIR__ . '/../inc/above.php';
 			<div class="points points-1 columns small-12 medium-6 large-3 large-offset-1">
 				<div class="title h4 strong space-25-bottom"><span class="text-red-2">High-Rise Safety</span> <br>Engineering</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
-					Early warning smoke detectors in your kitchen and living room. 
+					Early warning smoke detectors in your kitchen and living room.
 				</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
 					Smoke detectors trigger an early warning fire alarm with an exact indication of the location of a potential fire.
@@ -864,7 +852,7 @@ require_once __DIR__ . '/../inc/above.php';
 			</div>
 			<div class="points points-2 columns small-12 medium-6 medium-offset-6 large-3 large-offset-0">
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
-					In case a fire is to intense for early warning fire measures, evacuation is recommended. 
+					In case a fire is to intense for early warning fire measures, evacuation is recommended.
 				</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
 					A pressurised network of pipes feed the fire sprinklers in every room of your house.
@@ -874,10 +862,10 @@ require_once __DIR__ . '/../inc/above.php';
 					<div class="label strong text-neutral-2 text-uppercase space-min-top-bottom">Temperature Activated Sprinkler</div>
 				</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
-					Every fire sprinkler has a temperature activated glass bulb that melts at 68° Celsius. 
+					Every fire sprinkler has a temperature activated glass bulb that melts at 68° Celsius.
 				</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
-					Only the fire sprinklers in the region of the fire will activate when a fire melts the glass bulbs only. 
+					Only the fire sprinklers in the region of the fire will activate when a fire melts the glass bulbs only.
 				</div>
 				<div class="point h5 condensed text-neutral-3 space-min-bottom">
 					Triple redundant water pumps activate when they sense a drop in water pressure. This ensures the sprinklers are fed with a constant water pressure.
@@ -922,8 +910,8 @@ require_once __DIR__ . '/../inc/above.php';
 					<div tabindex="-1" class="h5 tab-button tab-button-large">November 2019</div>
 					<div tabindex="-1" class="h5 tab-button tab-button-large">October 2019</div>
 					<div tabindex="-1" class="h5 tab-button tab-button-large active">September 2019</div>
-				</div> 
-					
+				</div>
+
 				<select class="select-menu button strong fill-red-2 show-for-medium ">
 					<option>November 2019</option>
 					<option>October 2019</option>
