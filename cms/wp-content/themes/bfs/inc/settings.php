8d8e37eeb5a7e29af6ee47e7e7c36248b4c640be
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-01-15 10:57:58 +0530

    CMS : WordPress : Add support for Menus; so that it'll show up under Appearance

--- cms/wp-content/themes/bfs/inc/settings.php
+++ cms/wp-content/themes/bfs/inc/settings.php
@@ -50,11 +50,28 @@ acf_add_options_page( [
 ] );
 
 
+function bfs_theme_setup () {
 
-/*
- *
- * Media Settings
- *
- */
-add_theme_support( 'post-thumbnails' );
-add_image_size( 'small', 400, 400, false );
+	/*
+	 * Theme Supports
+	 *
+	 * Register support for certain features
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/
+	 */
+	// Enable support for Post Thumbnails on posts and pages.
+	// @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'menus' );
+
+
+	/*
+	 *
+	 * Media Settings
+	 *
+	 */
+	add_image_size( 'small', 400, 400, false );
+
+}
+
+add_action( 'after_setup_theme', 'bfs_theme_setup' );
