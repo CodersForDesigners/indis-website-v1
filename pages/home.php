ed20eca257e0cd495b172a9c600fa42a97e63696
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-01-17 14:13:46 +0530

    Home page : Project Listing : CMS-ify this section

--- pages/home.php
+++ pages/home.php
@@ -1,15 +1,16 @@
 <?php
 /*
  *
- * This is a sample page you can copy and use as boilerplate for any new page.
+ * The is the Home page
  *
  */
+require_once __DIR__ . '/../inc/above.php';
 
-// Page-specific preparatory code goes here.
+// In the context of this page,
+// 	`allProjectsExcludingCurrent` literally refers to "all the projects"
+$projects = &$allProjectsExcludingCurrent;
 
 ?>
-
-<?php require_once __DIR__ . '/../inc/above.php'; ?>
 
 
 
@@ -39,50 +40,19 @@
 					</div>
 				</div>
 			</div>
-			<div class="project-item-container columns small-12 medium-6 large-4">
-				<a href="/project-single" class="project-item block fill-neutral-2" tabindex="-1" style="background-image: url('../media/listing-cover/one-city.jpg<?php echo $ver ?>');">
-					<div class="project-card fill-dark space-25">
-						<div class="title h4 strong">One City, HYD</div>
-						<div class="location label">Kukatpally</div>
-						<hr class="dash">
-						<div class="type h6 strong space-25-top">2 & 3 BHK Apartments</div>
-						<div class="price h5 condensed space-min-top">50Lakhs to 1.3Cr</div>
-					</div>
-				</a>
-			</div>
-			<div class="project-item-container columns small-12 medium-6 large-4">
-				<a href="/project-single" class="project-item block fill-neutral-2" tabindex="-1" style="background-image: url('../media/listing-cover/pbel-city.jpg<?php echo $ver ?>');">
-					<div class="project-card fill-dark space-25">
-						<div class="title h4 strong">PBEL City, HYD</div>
-						<div class="location label">Appa Junction</div>
-						<hr class="dash">
-						<div class="type h6 strong space-25-top">2 & 3 BHK Apartments</div>
-						<div class="price h5 condensed space-min-top">80Lakhs to 1.5Cr</div>
-					</div>
-				</a>
-			</div>
-			<div class="project-item-container columns small-12 medium-6 large-4">
-				<a href="/project-single" class="project-item block fill-neutral-2" tabindex="-1" style="background-image: url('../media/listing-cover/vb-city.jpg<?php echo $ver ?>');">
-					<div class="project-card fill-dark space-25">
-						<div class="title h4 strong">VB City, HYD</div>
-						<div class="location label">Bolarum</div>
-						<hr class="dash">
-						<div class="type h6 strong space-25-top">2 & 3 BHK Apartments</div>
-						<div class="price h5 condensed space-min-top">40Lakhs to 80Lakhs</div>
-					</div>
-				</a>
-			</div>
-			<div class="project-item-container columns small-12 medium-6 large-4">
-				<a href="/project-single" class="project-item block fill-neutral-2" tabindex="-1" style="background-image: url('../media/listing-cover/some-city.jpg<?php echo $ver ?>');">
-					<div class="project-card fill-dark space-25">
-						<div class="title h4 strong">Some City, HYD</div>
-						<div class="location label">Kondapur</div>
-						<hr class="dash">
-						<div class="type h6 strong space-25-top">2 & 3 BHK Apartments</div>
-						<div class="price h5 condensed space-min-top">40Lakhs to 80Lakhs</div>
-					</div>
-				</a>
-			</div>
+			<?php foreach ( $projects as $project ) : ?>
+				<div class="project-item-container columns small-12 medium-6 large-4">
+					<a href="<?= $project[ 'permalink' ] ?>" class="project-item block fill-neutral-2" tabindex="-1" style="background-image: url( '<?= getContent( '', 'cover_images -> 0 -> sizes -> large', $project[ 'ID' ] ) ?>' );">
+						<div class="project-card fill-dark space-25">
+							<div class="title h4 strong"><?= $project[ 'post_title' ] ?></div>
+							<div class="location label"><?= getContent( '', 'location', $project[ 'ID' ] ) ?></div>
+							<hr class="dash">
+							<div class="type h6 strong space-25-top"><?= getContent( '', 'type', $project[ 'ID' ] ) ?></div>
+							<div class="price h5 condensed space-min-top"><?= getContent( '', 'price', $project[ 'ID' ] ) ?></div>
+						</div>
+					</a>
+				</div>
+			<?php endforeach; ?>
 		</div>
 	</div>
 </section>
