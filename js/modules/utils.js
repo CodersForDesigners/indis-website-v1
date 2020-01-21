082393a0450557ca0d7811d8bb398cfa1a7fa84a
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-01-21 18:06:20 +0530

    Utils : eventually : Add a function that eventually runs a given function

--- js/modules/utils.js
+++ js/modules/utils.js
@@ -13,3 +13,31 @@ function waitFor ( seconds ) {
 		}, seconds * 1000 );
 	} );
 }
+
+
+
+/*
+ *
+ * Returns a debounced function that eventually performs a given function
+ *
+ */
+function eventually ( fn, timeout ) {
+
+	var timeout = ( timeout || 1 ) * 1000;
+	var timeoutId = null;
+
+	return function ( event ) {
+
+		if ( timeoutId ) {
+			clearTimeout( timeoutId );
+			timeoutId = null;
+		}
+
+		return new Promise( function ( resolve, reject ) {
+			timeoutId = setTimeout( resolve, timeout );
+		} )
+			.then( fn );
+
+	};
+
+}
