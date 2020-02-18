2c08d633aeb63c20d84f54b809f7d4a61ae3090a
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-02-18 12:35:05 +0530

    Home : Projects : "Pop" each project as they enter view (2)
    Re-organize things
    Add some documentation

--- js/home.js
+++ js/home.js
@@ -1,8 +1,17 @@
 
 ( function () {
 
+/*
+ *
+ * Pop Project Tiles as they come **fully** into view
+ *
+ */
+	// This is an Intersection Observer handler; not gonna explain the mechanics of this
 	function inViewHandler ( entries, observer ) {
-		entries.forEach( function ( entry ) {
+		var _i;
+		var numEntries = entries.length;
+		for ( _i = 0; _i < numEntries; _i += 1 ) {
+			var entry = entries[ _i ];
 			var $element = $( entry.target );
 			// If it comes into view
 			if ( ! $element.data( "previous__isIntersecting" ) && entry.isIntersecting ) {
@@ -13,9 +22,10 @@
 				$element.removeClass( "in-view" );
 			}
 			$element.data( "previous__isIntersecting", entry.isIntersecting );
-		} );
+		}
 	}
 
+	// Set up the Intersection Observer
 	var observer = new IntersectionObserver( inViewHandler, {
 		root: null,
 		rootMargin: "0px",
@@ -23,7 +33,9 @@
 	} );
 
 	$( ".js_project_item" ).each( function ( _i, domProject ) {
+		// Set the default `isIntersecting` values for all the project tiles
 		$( domProject ).data( "previous__isIntersecting", false );
+		// Start "IntersectionObserve"-ing the project tiles
 		observer.observe( domProject );
 	} );
 
