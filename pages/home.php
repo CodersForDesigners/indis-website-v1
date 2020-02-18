b2ed86afbe5f3e832de1b798c958d5ae6a8b7eb3
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-02-18 13:35:04 +0530

    Home : Spotlights : Add this section

--- pages/home.php
+++ pages/home.php
@@ -10,6 +10,21 @@ require_once __DIR__ . '/../inc/above.php';
 // 	`allProjectsExcludingCurrent` literally refers to "all the projects"
 $projects = &$allProjectsExcludingCurrent;
 
+// Consolidate the featured spotlights across all projects
+$featuredSpotlights = [ ];
+foreach ( $projects as $project ) {
+	$projectSpotlights = getContent( [ ], 'spotlight_list', $project[ 'ID' ] );
+	foreach ( $projectSpotlights as $spotlight ) {
+		if ( ! $spotlight[ 'spotlight_featured' ] )
+			continue;
+		$spotlight[ 'project' ] = $project[ 'post_title' ];
+		$spotlight[ 'permalink' ] = $project[ 'permalink' ];
+		$featuredSpotlights[ ] = $spotlight;
+	}
+}
+shuffle( $featuredSpotlights );
+$numberOfSpotlights = str_pad( count( $featuredSpotlights ), 2, '0', STR_PAD_LEFT );
+
 ?>
 
 
@@ -58,6 +73,47 @@ $projects = &$allProjectsExcludingCurrent;
 	</div>
 </section>
 <!-- END: Project List Section -->
+
+
+<!-- Carousel: Offers -->
+<?php if ( ! empty( $featuredSpotlights ) ) : ?>
+<div data-section="Offers" id="spotlight" class="carousel indis-carousel js_navigation_sticky_point js_carousel_container">
+	<div class="carousel-list js_carousel_content">
+		<div class="carousel-list-item js_carousel_item">
+			<div class="carousel-title h2 strong">
+				<span class="text-red-2">spotlight</span> <br>apartment <br>units
+			</div>
+		</div>
+		<?php foreach ( $featuredSpotlights as $index => $spotlight ) : ?>
+			<div class="carousel-list-item js_carousel_item <?php if ( $index >= 4 ) echo 'hidden' ?>">
+				<div class="card-index text-neutral-2">
+					<div class="count h3 inline-bottom"><?= str_pad( $index + 1, 2, '0', STR_PAD_LEFT ) ?></div>
+					<div class="total label strong text-uppercase inline-bottom"><?= $numberOfSpotlights ?></div>
+				</div>
+				<div class="carousel-card fill-dark" style="background-image: url( '<?= $spotlight[ 'spotlight_thumbnail' ][ 'sizes' ][ 'small' ] ?>' );">
+					<div class="info space-25">
+						<div class="project label strong text-neutral-2"><?= $spotlight[ 'project' ] ?></div>
+						<div class="info-box">
+							<span class="title h5 strong"><?= $spotlight[ 'spotlight_title' ] ?></span>
+							<span class="description p text-neutral-2"><?= $spotlight[ 'spotlight_description' ] ?></span>
+						</div>
+						<div class="price h5 condensed"><?= $spotlight[ 'spotlight_price' ] ?></div>
+						<div class="time h6 condensed"><span class="text-uppercase">Valid For :</span> <?= getIntervalString( $spotlight[ 'spotlight_expiry' ] ) ?></div>
+					</div>
+				</div>
+				<a href="<?= $spotlight[ 'permalink' ] ?>" target="_blank" class="button fill-neutral-4 text-light button-icon" style="--bg-i: url('../media/icon/icon-right-triangle-light.svg<?php echo $ver ?>'); --bg-c: var(--neutral-2);">Visit Project</a>
+			</div>
+		<?php endforeach; ?>
+	</div>
+	<div class="carousel-controls clearfix">
+		<div class="container">
+			<div class="prev float-left"><button class="button fill-light js_pager" data-dir="left"><img class="block" src="../media/icon/icon-left-triangle-dark.svg<?php echo $ver ?>"></button></div>
+			<div class="next float-right"><button class="button fill-light js_pager" data-dir="right"><img class="block" src="../media/icon/icon-right-triangle-dark.svg<?php echo $ver ?>"></button></div>
+		</div>
+	</div>
+</div>
+<?php endif; ?>
+<!-- END: Carousel: Offers -->
 
 
 <?php require_once __DIR__ . '/../inc/below.php'; ?>
