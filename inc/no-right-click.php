0b78fe51c065437fa4df41c75198f0c5e0e339a8
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-03-25 10:44:48 +0530

    Snippet : Disable right-click : Add this one

---
+++ inc/no-right-click.php
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+	document.addEventListener( "contextmenu", function ( event ) {
+		event.preventDefault();
+		event.stopImmediatePropagation();
+		event.stopPropagation();
+		return false;
+	}, { capture: true } );
+</script>
