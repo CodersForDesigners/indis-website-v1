99c0cd385f251ff2c79ba38f9a60c48c5cfc57c7
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-11-25 11:14:59 +0530

    Content: Footer: Corporate Phone Number: Include this

--- inc/below.php
+++ inc/below.php
@@ -55,6 +55,8 @@
 					<div class="footer-address columns small-12 large-4 xlarge-3 xlarge-offset-1">
 						<div class="title h6 strong text-uppercase space-min-bottom">Corporate Address :</div>
 						<div class="address h4 text-neutral-2 space-min-bottom">Plot No. 825, Road No.44, <br>Jubilee Hills, Hyderabad, <br>Telangana — 500033.</div>
+						<div class="title h6 strong text-uppercase space-min-top space-min-bottom">Corporate Phone :</div>
+						<div class="address h4 text-neutral-2 space-min-bottom"><a href="tel:+914068989898" class="text-red-2">+91 40 6898 9898</a></div>
 						<a href="https://goo.gl/maps/jFcbmAJ1rhwwh5Wo6" target="_blank" class="hidden link label strong text-red-2 text-uppercase inline-middle">Open in Google Maps <img class="link-icon inline-middle" src="../media/icon/icon-location-color.svg<?php echo $ver ?>"></a>
 
 						<a href="mailto:corporateaffairs@indis.co.in" target="_blank" class="label strong text-neutral-4 inline-middle">
